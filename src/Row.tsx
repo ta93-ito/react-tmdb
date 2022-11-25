390b00abe842b894ca7fbca0fb64d64120665734
ta93-ito <tkm.ito@outlook.jp>
2022-11-26 05:33:04 +0900

    row component

---
+++ src/Row.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+type Props = {
+  title: string;
+  fetchUrl: string;
+  isLargeRow?: boolean;
+};
+
+type Movie = {
+  id: string;
+  name: string;
+  title: string;
+  original_name: string;
+  poster_path: string;
+  backdrop_path: string;
+};
+
+export const Row = ({ title, fetchUrl }: Props) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.log(movies);
+
+  return <div className="Row" />;
+};
